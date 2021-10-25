bf3e9cc19ddf323fdc44367f5be8e8b6de30d0cd
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2021-10-25 16:45:13 +0300

    Update TokenNames.java

--- ex1/src/TokenNames.java
+++ ex1/src/TokenNames.java
@@ -9,4 +9,25 @@ public interface TokenNames {
   public static final int RPAREN = 6;
   public static final int NUMBER = 7;
   public static final int ID = 8;
+  public static final int LBRACK = 9;
+  public static final int RBRACK = 10;
+  public static final int LBRACE = 11;
+  public static final int RBRACE = 12;
+  public static final int NIL = 13;
+  public static final int COMMA = 14;
+  public static final int DOT = 15;
+  public static final int SEMICOLON = 16;
+  public static final int ASSIGN = 17;
+  public static final int EQ = 18;
+  public static final int LT = 19;
+  public static final int GT = 20;
+  public static final int ARRAY = 21;
+  public static final int CLASS = 22;
+  public static final int EXTENDS = 23;
+  public static final int RETURN = 24;
+  public static final int WHILE = 25;
+  public static final int IF = 26;
+  public static final int NEW = 27;
+  public static final int INT = 28;
+  public static final int STRING = 29;
 }
