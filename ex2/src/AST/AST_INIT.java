9a06189da54c1dd5e32c51e5d99eaae4430cb2b9
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2021-11-22 11:49:06 +0200

    Create AST_INIT.java

---
+++ ex2/src/AST/AST_INIT.java
@@ -0,0 +1,50 @@
+package AST;
+
+public class AST_INIT extends AST_Node
+{
+	public AST_PRO pro;
+  
+  public AST_INIT(AST_PRO pro)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		if (pro != null) System.out.print("====================== init -> pro\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.pro = pro;
+	}
+  
+  public void PrintMe()
+	{
+		/**************************************/
+		/* AST NODE TYPE = AST PRO */
+		/**************************************/
+		System.out.print("AST NODE INIT\n");
+
+		/*************************************/
+		/* RECURSIVELY PRINT HEAD + TAIL ... */
+		/*************************************/
+		if (pro != null) pro.PrintMe();
+
+		/**********************************/
+		/* PRINT to AST GRAPHVIZ DOT file */
+		/**********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			"PRO\n");
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (pro != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,pro.SerialNumber);
+	}
+  
+}
