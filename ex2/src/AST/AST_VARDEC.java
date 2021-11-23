91347eec66b27dabb0befe9cde34fc53ccfbff46
eyalban <79798517+eyalban@users.noreply.github.com>
2021-11-23 16:31:39 +0200

    Update AST_VARDEC.java

--- ex2/src/AST/AST_VARDEC.java
+++ ex2/src/AST/AST_VARDEC.java
@@ -5,8 +5,10 @@ public class AST_VARDEC extends AST_Node
   public AST_TYPE t;
 	public AST_Node exp;
   public String s;
+  public int is_new;
   
-  public AST_VARDEC(AST_TYPE t, String s, AST_Node exp)
+  
+  public AST_VARDEC(AST_TYPE t, String s, AST_Node exp, int is_new)
 	{
 		/******************************/
 		/* SET A UNIQUE SERIAL NUMBER */
@@ -16,8 +18,11 @@ public class AST_VARDEC extends AST_Node
 		/***************************************/
 		/* PRINT CORRESPONDING DERIVATION RULE */
 		/***************************************/
-		if (exp != null) System.out.print("====================== varDec --> 2/3\n");
-		if (exp == null) System.out.print("====================== varDec --> 1\n");
+		if (exp != null){
+			if (is_new == 0) System.out.print("====================== varDec --> type ID := exp;");
+			if (is_new == 1) System.out.print("====================== varDec --> type ID := newExp;");
+		}
+		if (exp == null) System.out.print("====================== varDec --> type ID;");
 
 		/*******************************/
 		/* COPY INPUT DATA NENBERS ... */
@@ -25,6 +30,7 @@ public class AST_VARDEC extends AST_Node
 		this.t = t;
 		this.s = s;
     this.exp = exp;
+    this.is_new = is_new
 	}
   
   public void PrintMe()
@@ -38,7 +44,13 @@ public class AST_VARDEC extends AST_Node
 		/* RECURSIVELY PRINT HEAD + TAIL ... */
 		/*************************************/
 		if (t != null) t.PrintMe();
-		if (exp != null) exp.PrintMe();
+		System.out.print(s);
+		if (exp != null){
+			System.out.print(" := ");
+			exp.PrintMe();
+		}
+		System.out.print(";");
+		
 
 		/**********************************/
 		/* PRINT to AST GRAPHVIZ DOT file */
