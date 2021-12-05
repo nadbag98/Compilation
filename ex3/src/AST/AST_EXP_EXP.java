757b421fc8ad64b707c9c331fe9a51861b57883b
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2021-12-05 17:21:13 +0200

    Update AST_EXP_EXP.java

--- ex3/src/AST/AST_EXP_EXP.java
+++ ex3/src/AST/AST_EXP_EXP.java
@@ -1,9 +1,10 @@
+
 package AST;
 
 public class AST_EXP_EXP extends AST_EXP
 {
   public AST_EXP e1;
-	public AST_BINOP b;
+public AST_BINOP b;
   public AST_EXP e2;
   
   public AST_EXP_EXP(AST_EXP e1, AST_BINOP b, AST_EXP e2, int line)
@@ -27,7 +28,6 @@ public class AST_EXP_EXP extends AST_EXP
 		this.b = b;
     		this.e2 = e2;
     		this.line = line - 1;
-		System.out.print(String.format("HERRREEEEEEEEEEE: %d", this.line));
 	}
   
   public void PrintMe()
@@ -56,6 +56,54 @@ public class AST_EXP_EXP extends AST_EXP
 		if (e1 != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,e1.SerialNumber);
 		if (b != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,b.SerialNumber);
 		if (e2 != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,e2.SerialNumber);
+	}
+
+
+	public TYPE visit(SYMBOL_TABLE sym_table) throws ArithmeticException {
+		if (this.b != null){
+			TYPE t1 = null;
+			TYPE t2 = null;
+			
+			if (this.e1  != null) t1 = this.e1.visit(sym_table);
+			if (this.e2 != null) t2 = this.e2.visit(sym_table);
+			
+			if (this.b.op == 1){
+				if ((t1 == TYPE_INT.getInstance()) && (t2 == TYPE_INT.getInstance()))
+				{
+					return TYPE_INT.getInstance();
+				}
+
+				if ((t1 == TYPE_STRING.getInstance()) && (t2 == TYPE_STRING.getInstance()))
+				{
+					return TYPE_STRING.getInstance();
+				}
+			} else if ((this.b.op == 2) || (this.b.op == 3) || (this.b.op == 5) || (this.b.op == 6)){
+				
+				if ((t1 == TYPE_INT.getInstance()) && (t2 == TYPE_INT.getInstance()))
+				{
+					return TYPE_INT.getInstance();
+				}
+			} else if (this.b.op == 4){
+
+				if ((t1 == TYPE_INT.getInstance()) && (t2 == TYPE_INT.getInstance()) && (e2.isZero == 0))
+				{
+					return TYPE_INT.getInstance();
+				}
+			} else { // this.b.op == 7
+
+				//complicated type compate hereeeee for =
+			}
+
+			throw new ArithmeticException(String.format("%d", this.line)); 
+			return null;
+		}
+
+		else {
+
+			return this.e1.visit(sym_table);
+
+		}		
+
 	}
   
 }
