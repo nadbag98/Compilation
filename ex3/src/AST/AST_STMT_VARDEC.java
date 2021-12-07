7b05554061abc3e9d9e5b0aaa0586724a8934abd
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2021-12-07 16:57:59 +0200

    Create AST_STMT_VARDEC.java

--- ex3/src/AST/AST_STMT_VARDEC.java
+++ ex3/src/AST/AST_STMT_VARDEC.java
@@ -10,7 +10,7 @@ public class AST_STMT_VARDEC extends AST_STMT
 		/* SET A UNIQUE SERIAL NUMBER */
 		/******************************/
 		SerialNumber = AST_Node_Serial_Number.getFresh();
-		this.line = line - 1;
+		this.line = line;
 
 		/***************************************/
 		/* PRINT CORRESPONDING DERIVATION RULE */
