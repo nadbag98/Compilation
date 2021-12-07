f46af406384acf492279039b5c71baa7599ed3f9
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2021-12-07 16:58:18 +0200

    Update AST_STMT_LIST.java

--- ex3/src/AST/AST_STMT_LIST.java
+++ ex3/src/AST/AST_STMT_LIST.java
@@ -17,7 +17,7 @@ public class AST_STMT_LIST extends AST_Node
 		/* SET A UNIQUE SERIAL NUMBER */
 		/******************************/
 		SerialNumber = AST_Node_Serial_Number.getFresh();
-		this.line = line - 1;
+		this.line = line;
 
 		/***************************************/
 		/* PRINT CORRESPONDING DERIVATION RULE */
