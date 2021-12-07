75671fbb1fb79e4099eb9c426c170cb7c0bfb7f4
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2021-12-07 16:56:02 +0200

    Update AST_VARDEC.java

--- ex3/src/AST/AST_VARDEC.java
+++ ex3/src/AST/AST_VARDEC.java
@@ -14,7 +14,7 @@ public class AST_VARDEC extends AST_Node
 		/* SET A UNIQUE SERIAL NUMBER */
 		/******************************/
 		SerialNumber = AST_Node_Serial_Number.getFresh();
-		this.line = line - 1;
+		this.line = line;
 
 		/***************************************/
 		/* PRINT CORRESPONDING DERIVATION RULE */
