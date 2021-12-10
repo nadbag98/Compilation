ad1833dd973ff22d4eb126c071db775760cf823a
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2021-12-10 11:46:30 +0200

    Update DATA_MEMBER_LIST.java

--- ex3/src/TYPES/DATA_MEMBER_LIST.java
+++ ex3/src/TYPES/DATA_MEMBER_LIST.java
@@ -9,5 +9,22 @@ public class DATA_MEMBER_LIST
 	{
 		this.head = head;
 		this.tail = tail;
-	}	
+	}
+	
+	public DATA_MEMBER find(String name)
+	{
+		if (this.head == null)
+		{
+			return null;
+		}
+		if (this.head.name.equals(name))
+		{
+			return this.head;
+		}
+		if (this.tail == null)
+		{
+			return null;
+		}
+		return this.tail.find(name);
+	}
 }
