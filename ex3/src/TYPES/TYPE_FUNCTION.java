dac7e50ac5f0ceb05e964d5db0905abf7a260a88
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2021-12-10 13:37:40 +0200

    Update TYPE_FUNCTION.java

--- ex3/src/TYPES/TYPE_FUNCTION.java
+++ ex3/src/TYPES/TYPE_FUNCTION.java
@@ -24,5 +24,37 @@ public class TYPE_FUNCTION extends TYPE
 	
 	public boolean isFunc(){ return true;}
 	
-	public 
+	public boolean equals(TYPE other){
+		if (!other.isFunc()){
+			return false;
+		}
+		
+		TYPE_FUNCTION otherFunc = (TYPE_FUNCTION)other;
+		
+		if (this.returnType != otherFunc.returnType){
+			return false;
+		}
+		
+		TYPE_LIST L1 = this.params;
+		TYPE_LIST L2 = otherFunc.params;
+		
+		if (L1.head != L2.head){
+			return false;
+		}
+		
+		while (L1.head != null && L2.head != null){
+			if (L1.head != L2.head){
+				return false;
+			}
+			L1 = L1.tail;
+			L2 = L2.tail;
+		}
+		
+		if (L1.head == L2.head){
+			return true;
+		}
+		
+		return false;
+		
+	}
 }
