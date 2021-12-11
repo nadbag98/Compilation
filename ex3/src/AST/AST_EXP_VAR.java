adc1a2640bec4fc4647e5c6e197b12ad49bf0275
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2021-12-11 17:04:59 +0200

    Update AST_EXP_VAR.java

--- ex3/src/AST/AST_EXP_VAR.java
+++ ex3/src/AST/AST_EXP_VAR.java
@@ -87,7 +87,7 @@ public class AST_EXP_VAR extends AST_EXP
 		} else {
 			TYPE_LIST lst = new TYPE_LIST(null, null);
 			this.l.visit(sym_table, lst);
-			if (!lst.equals(func.params)){
+			if (!func.params.equalsOrFamily(lst)){
 				System.out.print("Forth error in visit AST_EXP_VAR");
 				throw new ArithmeticException(String.format("%d", this.line));
 			}
