26ce3646a8e6cff83f4bea965a5d8f0c9fcd678d
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2021-12-11 17:36:30 +0200

    Create AST_CLASS_DUMMY.java

---
+++ ex3/src/AST/AST_CLASS_DUMMY.java
@@ -0,0 +1,64 @@
+package AST;
+import SYMBOL_TABLE.*;
+import TYPES.*;
+
+public class AST_EXP_REST extends AST_Node
+{
+  public String s;
+  
+  public AST_EXP_REST(String s, int line)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+    if (s == null) System.out.print("====================== exp --> NIL\n");
+    if (s != null) System.out.print("====================== exp --> STRING\n");
+
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.s = s;
+		this.line = line;
+	}
+  
+  public void PrintMe()
+	{
+		/**************************************/
+		/* AST NODE TYPE = AST EXP_REST */
+		/**************************************/
+		System.out.print("AST NODE EXP_REST\n");
+
+		/*************************************/
+		/* RECURSIVELY PRINT HEAD + TAIL ... */
+		/*************************************/
+		if (s != null) System.out.print(s);
+    if (s == null) System.out.print("NIL");
+
+		/**********************************/
+		/* PRINT to AST GRAPHVIZ DOT file */
+		/**********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber, "EXP_REST\n");
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+	}
+	
+	public TYPE visit(SYMBOL_TABLE sym_table) throws ArithmeticException {
+		System.out.print("Visiting AST_EXP_REST\n");
+	
+		if (this.s == null){
+			return TYPE_NIL.getInstance();
+		} else {
+			return TYPE_STRING.getInstance();
+		}
+	}
+  
+}
