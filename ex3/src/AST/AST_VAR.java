5f749e6c2a5dc272b11bcea42c3488373e49082c
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2021-12-11 12:44:34 +0200

    Update AST_VAR.java

--- ex3/src/AST/AST_VAR.java
+++ ex3/src/AST/AST_VAR.java
@@ -2,8 +2,8 @@ package AST;
 
 public class AST_VAR extends AST_Node
 {
-  public AST_VAR v;
-  public String s;
+  	public AST_VAR v;
+  	public String s;
 	public AST_EXP e;
   
   public AST_VAR(AST_VAR v, String s, AST_EXP e, int line)
@@ -26,7 +26,7 @@ public class AST_VAR extends AST_Node
 		/*******************************/
 		this.v = v;
 		this.s = s;
-    this.e = e;
+    		this.e = e;
 	}
   
   public void PrintMe()
@@ -53,6 +53,36 @@ public class AST_VAR extends AST_Node
 		/****************************************/
 		if (v != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,v.SerialNumber);
 		if (e != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,e.SerialNumber);
+	}
+	
+	public TYPE visit(SYMBOL_TABLE sym_table) throws ArithmeticException {
+		if (this.v == null && this.e == null) {
+			TYPE t1 = sym_table.searchAll(this.s);
+			if (t1 == null) {
+				throw new ArithmeticException(String.format("%d", this.line));
+			}
+			return t1;
+		}
+		TYPE t1 = this.v.visit(sym_table);
+		if (this.e == null) {
+			if (!t1.isClass()) {
+				throw new ArithmeticException(String.format("%d", this.line));
+			}
+			TYPE t2 = sym_table.searchFamily(this.s, (TYPE_CLASS) t1);
+			if (t2 == null) {
+				throw new ArithmeticException(String.format("%d", this.line));
+			}
+			return t2;
+		}
+		if (!t1.isArray()) {
+			throw new ArithmeticException(String.format("%d", this.line));
+		}
+		TYPE t2 = this.e.visit(sym_table);
+		if (!t2 == TYPE_INT.getInstance()) {
+			throw new ArithmeticException(String.format("%d", this.line));
+		}
+		return t1.arrayType;
+		
 	}
   
 }
