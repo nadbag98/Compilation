d93edb03a76334e95144b76c7b5c35ed54a865fc
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2021-12-21 16:55:46 +0200

    Add files via upload

---
+++ ex4/TYPES/DATA_MEMBER.java
@@ -0,0 +1,13 @@
+package TYPES;
+
+public class DATA_MEMBER
+{
+	public TYPE t;
+	public String name;
+	
+	public DATA_MEMBER(TYPE t, String name)
+	{
+		this.t = t;
+		this.name = name;
+	}
+}
---
+++ ex4/TYPES/DATA_MEMBER_LIST.java
@@ -0,0 +1,45 @@
+package TYPES;
+
+public class DATA_MEMBER_LIST
+{
+	public DATA_MEMBER head;
+	public DATA_MEMBER_LIST tail;
+	
+	public DATA_MEMBER_LIST(DATA_MEMBER head,DATA_MEMBER_LIST tail)
+	{
+		this.head = head;
+		this.tail = tail;
+	}
+	
+	public DATA_MEMBER find(String name)
+	{
+		if (this.head == null)
+		{
+			return null;
+		}
+		if (this.head.name.equals(name))
+		{
+			return this.head;
+		}
+		if (this.tail == null)
+		{
+			return null;
+		}
+		return this.tail.find(name);
+	}
+	
+	public void insert(DATA_MEMBER d)
+	{
+		if (this.head == null)
+		{
+			this.head = d;
+			return;
+		}
+		if (this.tail == null)
+		{
+			this.tail = new DATA_MEMBER_LIST(d, null);
+			return;
+		}
+		this.tail.insert(d);
+	}
+}
