bb48f3b57de626ff02955e5a022561e1304f5a8d
eyalban <79798517+eyalban@users.noreply.github.com>
2022-01-24 13:02:07 +0200

    Update AST_FUNCDEC.java

--- ex4/src/AST/AST_FUNCDEC.java
+++ ex4/src/AST/AST_FUNCDEC.java
@@ -62,23 +62,30 @@ public class AST_FUNCDEC extends AST_Node
 	}
 	
     public TYPE visit(SYMBOL_TABLE sym_table, TYPE_CLASS my_class, boolean in_class) throws ArithmeticException {
-		if (sym_table.searchCurrScope(this.s)){
+		if (sym_table.searchCurrScope(this.s))
+		{
 			System.out.print("Exception in AST_FUNCDEC - Failed searchCurrScope\n");
 			throw new ArithmeticException(String.format("%d", t.line)); 
 		}
+		
 		TYPE ret_val = sym_table.findType(this.t.s);
-		if (ret_val == null) {
+		if (ret_val == null)
+		{
 			System.out.print("Exception in AST_FUNCDEC - Failed findType\n");
 			throw new ArithmeticException(String.format("%d", t.line));
 		}
+		
 		TYPE_LIST param_list = new TYPE_LIST(null, null);
 		TYPE_FUNCTION func = new TYPE_FUNCTION(ret_val, this.s, param_list);
 		sym_table.enter(this.s, func, null);
 		sym_table.beginScope();
+		
 		if (this.l1 != null) {
 			l1.visit(sym_table, func.params);
 		}
+		
 		if (in_class) {
+			boolean inherited = false;
 			TYPE_CLASS ancestor = my_class.father;
 			DATA_MEMBER dup;
 			while(ancestor != null) {
@@ -87,10 +94,15 @@ public class AST_FUNCDEC extends AST_Node
 					System.out.print("Exception in AST_FUNCDEC - Failed dup memebers.find()\n");
 					throw new ArithmeticException(String.format("%d", t.line));
 				}
+				if (dup != null){
+					inherited = true;
+				}
 				ancestor = ancestor.father;
 			}
-			DATA_MEMBER d = new DATA_MEMBER(func, this.s);
-			my_class.data_members.insert(d);
+			if (!inherited){
+				DATA_MEMBER d = new DATA_MEMBER(func, this.s, my_class.data_members.getFuncOffset());
+				my_class.data_members.insert(d);
+			}
 		}
 		l2.visit(sym_table, func.returnType);
 		sym_table.endScope();
