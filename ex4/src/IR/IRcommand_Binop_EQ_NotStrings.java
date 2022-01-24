ed500f16d937193892b64818f1609baef20f4c45
eyalban <79798517+eyalban@users.noreply.github.com>
2022-01-24 14:59:42 +0200

    Update and rename IRcommand_Binop_EQ_Integers.java to IRcommand_Binop_EQ_NotStrings.java

--- ex4/src/IR/IRcommand_Binop_EQ_NotStrings.java
+++ ex4/src/IR/IRcommand_Binop_EQ_NotStrings.java
@@ -13,13 +13,13 @@ package IR;
 import TEMP.*;
 import MIPS.*;
 
-public class IRcommand_Binop_EQ_Integers extends IRcommand
+public class IRcommand_Binop_EQ_NotStrings extends IRcommand
 {
 	public TEMP t1;
 	public TEMP t2;
 	public TEMP dst;
 
-	public IRcommand_Binop_EQ_Integers(TEMP dst,TEMP t1,TEMP t2)
+	public IRcommand_Binop_EQ_NotStrings(TEMP dst,TEMP t1,TEMP t2)
 	{
 		this.dst = dst;
 		this.t1 = t1;
