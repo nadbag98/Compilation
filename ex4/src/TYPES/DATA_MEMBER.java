01b40de13ccdf382c66326b5c93b1922b5d034a5
eyalban <79798517+eyalban@users.noreply.github.com>
2022-01-24 10:21:56 +0200

    Update DATA_MEMBER.java

--- ex4/src/TYPES/DATA_MEMBER.java
+++ ex4/src/TYPES/DATA_MEMBER.java
@@ -4,10 +4,12 @@ public class DATA_MEMBER
 {
 	public TYPE t;
 	public String name;
+	public int offset;
 	
-	public DATA_MEMBER(TYPE t, String name)
+	public DATA_MEMBER(TYPE t, String name, int offset)
 	{
 		this.t = t;
 		this.name = name;
+		this.offset = offset;
 	}
 }
