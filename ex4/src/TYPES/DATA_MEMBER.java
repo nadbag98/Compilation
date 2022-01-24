07537c13446dd52391b383b458f8cdd91e8d9481
eyalban <79798517+eyalban@users.noreply.github.com>
2022-01-24 16:34:01 +0200

    Update DATA_MEMBER.java

--- ex4/src/TYPES/DATA_MEMBER.java
+++ ex4/src/TYPES/DATA_MEMBER.java
@@ -5,11 +5,18 @@ public class DATA_MEMBER
 	public TYPE t;
 	public String name;
 	public int offset;
+	public String defClass;
 	
-	public DATA_MEMBER(TYPE t, String name, int offset)
+	public DATA_MEMBER(TYPE t, String name, int offset, String defClass)
 	{
 		this.t = t;
 		this.name = name;
 		this.offset = offset;
+		this.defClass = defClass;
+	}
+	
+	public DATA_MEMBER copy(){
+		DATA_MEMBER d = new DATA_MEMBER(this.t, this.name, this.offset, this.defClass);
+		return d;
 	}
 }
