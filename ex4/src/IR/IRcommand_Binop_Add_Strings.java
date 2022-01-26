21573197d9d0b0b94ee9c7aced33e241ac52e50c
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2022-01-26 18:06:57 +0200

    Update IRcommand_Binop_Add_Strings.java

--- ex4/src/IR/IRcommand_Binop_Add_Strings.java
+++ ex4/src/IR/IRcommand_Binop_Add_Strings.java
@@ -30,6 +30,15 @@ public class IRcommand_Binop_Add_Strings extends IRcommand
 	/***************/
 	public void MIPSme()
 	{
-		MIPSGenerator.getInstance().addStrings(dst,t1,t2);
+		String str1_len_loop= IRcommand.getFreshLabel("str1_len_loop"); 
+		String str1_len_end= IRcommand.getFreshLabel("str1_len_end"); 
+		String str2_len_loop= IRcommand.getFreshLabel("str2_len_loop"); 
+		String str2_len_end= IRcommand.getFreshLabel("str2_len_end"); 
+		String copy_str1_loop= IRcommand.getFreshLabel("copy_str1_loop");
+		String copy_str1_end= IRcommand.getFreshLabel("copy_str1_end"); 
+		String copy_str2_loop= IRcommand.getFreshLabel("copy_str2_loop"); 
+		String copy_str2_end= IRcommand.getFreshLabel("copy_str2_end");
+		MIPSGenerator.getInstance().addStrings(dst,t1,t2, str1_len_loop, str1_len_end, str2_len_loop, str2_len_end, copy_str1_loop,
+			 	copy_str1_end, copy_str2_loop, copy_str2_end);
 	}
 }
