2e1a298ae6efc1db1dc1963743b521d8da22d55d
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2022-01-26 16:44:25 +0200

    Create IRcommand_StoreTemp2Temp.java

---
+++ ex4/src/IR/IRcommand_StoreTemp2Temp.java
@@ -0,0 +1,34 @@
+/***********/
+/* PACKAGE */
+/***********/
+package IR;
+
+/*******************/
+/* GENERAL IMPORTS */
+/*******************/
+
+/*******************/
+/* PROJECT IMPORTS */
+/*******************/
+import TEMP.*;
+import MIPS.*;
+
+public class IRcommand_StoreTemp2Temp extends IRcommand
+{
+	TEMP dst;
+	TEMP src;
+	
+	public IRcommand_StoreTemp2Temp(TMP dst,TEMP src)
+	{
+		this.src      = src;
+		this.dst = dst;
+	}
+	
+	/***************/
+	/* MIPS me !!! */
+	/***************/
+	public void MIPSme()
+	{
+		MIPSGenerator.getInstance().store(src, dst);
+	}
+}
