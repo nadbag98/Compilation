5042325ba199069641b359d91ed9372d0112d7af
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2022-01-26 13:42:46 +0200

    Update AST_STMT_EXP.java

--- ex4/src/AST/AST_STMT_EXP.java
+++ ex4/src/AST/AST_STMT_EXP.java
@@ -76,6 +76,61 @@ public class AST_STMT_EXP extends AST_STMT
 		}		
 		
 		return null;
-	}	
+	}
+	
+	public TEMP IRme()
+	{
+	
+		if (this.l != null && this.is_while == 1){
+			/*******************************/
+			/* [1] Allocate 2 fresh labels */
+			/*******************************/
+			String label_end   = IRcommand.getFreshLabel("end");
+			String label_start = IRcommand.getFreshLabel("start");
+
+			/*********************************/
+			/* [2] entry label for the while */
+			/*********************************/
+			IR.
+			getInstance().
+			Add_IRcommand(new IRcommand_Label(label_start));
+
+			/********************/
+			/* [3] cond.IRme(); */
+			/********************/
+			TEMP cond_temp = this.e.IRme();
+
+			/******************************************/
+			/* [4] Jump conditionally to the loop end */
+			/******************************************/
+			IR.
+			getInstance().
+			Add_IRcommand(new IRcommand_Jump_If_Eq_To_Zero(cond_temp,label_end));		
+
+			/*******************/
+			/* [5] body.IRme() */
+			/*******************/
+			this.l.IRme();
+
+			/******************************/
+			/* [6] Jump to the loop entry */
+			/******************************/
+			IR.
+			getInstance().
+			Add_IRcommand(new IRcommand_Jump_Label(label_start));		
+
+			/**********************/
+			/* [7] Loop end label */
+			/**********************/
+			IR.
+			getInstance().
+			Add_IRcommand(new IRcommand_Label(label_end));
+
+			/*******************/
+			/* [8] return null */
+			/*******************/
+			return null;
+		}
+	}
   
 }
