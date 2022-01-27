2f8cd89cf06f72a797dec5d1e3d4a5b88693e3ad
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2022-01-27 14:59:06 +0200

    Create IRcommand_MulStringStringInt.java

---
+++ ex4/src/IR/IRcommand_MulStringStringInt.java
@@ -0,0 +1,35 @@
+/***********/
+/* PACKAGE */
+/***********/
+package IR;
+
+/*******************/
+/* GENERAL IMPORTS */
+/*******************/
+
+/*******************/
+/* PROJECT IMPORTS */
+/*******************/
+import TEMP.*;
+import MIPS.*;
+
+public class IRcommand_MulStringStringInt extends IRcommand
+{
+	public String dst;
+	public String op1;
+	public int op2;
+	
+	public IRcommand_MulStringStringInt(String dst, String op1, int op2)
+	{
+		this.dst = dst;
+		this.op1 = op1;
+		this.op2 = op2;
+	}
+	/***************/
+	/* MIPS me !!! */
+	/***************/
+	public void MIPSme()
+	{
+		MIPSGenerator.getInstance().mul(dst,op1,op2);
+	}
+}
