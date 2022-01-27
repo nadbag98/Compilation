52dda2bc86a0f40f8fae1c9ae97250be1adc60f8
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2022-01-27 12:05:38 +0200

    Create Class_Field.java

---
+++ ex4/src/AST/Class_Field.java
@@ -0,0 +1,15 @@
+/***********/
+/* PACKAGE */
+/***********/
+package AST;
+
+public class Class_Field
+{
+  int offset;
+  int int_val;
+  String string_val;
+  
+  public Class_Field(int offset, int int_val, String string_val){
+    
+  }
+}
