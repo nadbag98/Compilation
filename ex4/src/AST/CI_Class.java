d435b9c5ecce645c6e495f4501d08d61d14e500d
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2022-01-27 11:53:36 +0200

    Update CI_Class.java

--- ex4/src/AST/CI_Class.java
+++ ex4/src/AST/CI_Class.java
@@ -7,12 +7,12 @@ public class CI_Class
 {
   int c_size;
   String c_name;
-  Class_Fields_List c_list;
+  Class_Field_List c_list;
   
   public CI_Class(String c_name){
     this.c_size = 1;
     this.c_name = c_name;
-    this.c_list = new Class_Fields_List(null, null);
+    this.c_list = new Class_Field_List(null, null);
   }
   
   
