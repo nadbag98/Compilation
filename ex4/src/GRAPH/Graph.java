06c34afbe79ecb4ab7d5a8602a7922dfeb2fe7cb
eyalban <79798517+eyalban@users.noreply.github.com>
2022-01-31 16:25:15 +0200

    Update Graph.java

--- ex4/src/GRAPH/Graph.java
+++ ex4/src/GRAPH/Graph.java
@@ -3,6 +3,8 @@
 /***********/
 package GRAPH;
 
+import java.io.*;
+
 /*******************/
 /* GENERAL IMPORTS */
 /*******************/
@@ -14,6 +16,7 @@ package GRAPH;
 public class Graph
 {
 
+
  public NodeList nodes;
 	
 	public Graph() {
@@ -21,6 +24,18 @@ public class Graph
   }
   
   public void createBlocks(){
-    
+	  int counter = 1;
+   	 BufferedReader reader;
+	 reader = new BufferedReader(new FileReader("..\..\output\MIPS.txt"));
+	  String line = reader.readLine();
+	  while(line != null){
+		  if (line.contains(":") && !line.contains(".word") && !line.contains(".asciiz")) {
+			this.nodes.add(counter, line.substring(0, line.length()-1));
+		  }
+		  
+		  line = reader.readLine();
+		  counter++;
+	  }
+	  reader.close();
   }
 }
