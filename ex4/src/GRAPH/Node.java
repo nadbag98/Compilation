2d5475429f4cec6b9be0830f68d70105f567dd59
eyalban <79798517+eyalban@users.noreply.github.com>
2022-01-31 16:50:42 +0200

    Update Node.java

--- ex4/src/GRAPH/Node.java
+++ ex4/src/GRAPH/Node.java
@@ -3,12 +3,14 @@ public class Node
   public NodeList before;
   public NodeList after;
   public int line;
+  public int lastLine;
   public String label;
   
 	public Node(int line, String label) {
     this.before = new NodeList(null, null);
     this.after = new NodeList(null, null);
     this.line = line;
+	this.LastLine = line;
     this.label = label;
   }
 }
