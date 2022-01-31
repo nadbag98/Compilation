3f1cd8e93c5e3fc55062f06b2be38eca1c870e12
eyalban <79798517+eyalban@users.noreply.github.com>
2022-01-31 17:58:25 +0200

    Update Graph.java

--- ex4/src/GRAPH/Graph.java
+++ ex4/src/GRAPH/Graph.java
@@ -18,46 +18,88 @@ public class Graph
 
 
  public NodeList nodes;
-	
-	public Graph() {
+  
+  public Graph() {
     this.nodes = new NodeList(null, null);
   }
   
   public void createBlocks(){
-	  Node prevNode = null;
-	  int counter = 1;
-   	 BufferedReader reader;
-	 reader = new BufferedReader(new FileReader("..\..\output\MIPS.txt"));
-	  String line = reader.readLine();
-	  while(line != null){
-		  if (line.contains(":") && !line.contains(".word") && !line.contains(".asciiz")) {
-			  if (prevNode != null){
-				  prevNode.lastLine = counter-1;
-			  }
-			prevNode = this.nodes.add(counter, line.substring(0, line.length()-1));
-		  }
-		  else if(line.contains("\tj ") || line.contains("\tjal") || line.contains("\tblt") || line.contains("\tbgt") || line.containts("\tbge") ||
-			  line.contains("\tble") || line.contains("\tbne") || line.contains("\tbeq")){
-				line = reader.readLine();
-			  	counter++;
-			  	if (prevNode != null){
-				  prevNode.lastLine = counter-1;
-				  }
-			  	if (line.contains(":"){
-					prevNode = this.nodes.add(counter, line.substring(0, line.length()-1));
-				}
-				else{
-					prevNode = this.nodes.add(counter, null);	
-				}
-		  }
-		  else if(line.contains("\tjr")){
-			  prevNode.lastLine = counter;
-			prevNode = null;	  
-		  }
-		  
-		  line = reader.readLine();
-		  counter++;
-	  }
-	  reader.close();
+    Node prevNode = null;
+    int counter = 1;
+     BufferedReader reader;
+   reader = new BufferedReader(new FileReader("../../output/MIPS.txt"));
+    String line = reader.readLine();
+    while(line != null){
+      if (line.contains(":") && !line.contains(".word") && !line.contains(".asciiz")) {
+        if (prevNode != null){
+          prevNode.lastLine = counter-1;
+        }
+
+        prevNode = new Node(counter, line.substring(0, line.length()-1));
+        this.nodes.add(prevNode);
+
+      }
+      else if(line.contains("\tj ") || line.contains("\tblt") || line.contains("\tbgt") || line.containts("\tbge") ||
+        line.contains("\tble") || line.contains("\tbne") || line.contains("\tbeq")){
+        line = reader.readLine();
+          counter++;
+          if (prevNode != null){
+          prevNode.lastLine = counter-1;
+          }
+          if (line.contains(":"){
+            prevNode = new Node(counter, line.substring(0, line.length()-1));
+            this.nodes.add(prevNode);
+        }
+        else{
+          prevNode = new Node(counter, null);
+          this.nodes.add(prevNode); 
+
+        }
+      }
+      else if(line.contains("\tjr")){
+        prevNode.lastLine = counter;
+      prevNode = null;    
+      }
+      
+      line = reader.readLine();
+      counter++;
+    }
+    reader.close();
+  }
+            
+  public void createEdges(){
+    int counter = 1;
+    BufferedReader reader;
+    reader = new BufferedReader(new FileReader("../../output/MIPS.txt"));
+    String line = reader.readLine();
+    while(line != null){
+
+      if(line.contains("\tblt") || line.contains("\tbgt") || line.containts("\tbge") ||
+        line.contains("\tble") || line.contains("\tbne") || line.contains("\tbeq")){
+        Node src = this.nodes.findByLast(counter);
+        int idx = line.lastIndexOf(",");
+        String label = line.subString(idx+1, line.length());
+        Node dst = this.nodes.findByLabel(label);
+        src.after.add(dst);
+        dst.before.add(src);
+
+        dst = this.nodes.findByFirst(counter+1);
+        src.after.add(dst);
+        dst.before.add(src);
+      }
+
+      else if(line.contains("\tj ")){
+        Node src = this.nodes.findByLast(counter);
+        String label = line.subString(3, line.length());
+        Node dst = this.nodes.findByLabel(label);
+
+        src.after.add(dst);
+        dst.before.add(src);
+      }
+      
+      line = reader.readLine();
+      counter++;
+    }
+    reader.close();
   }
 }
