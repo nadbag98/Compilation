39ecc3274e22f90cf74a14a0c8c8237760a7f5de
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2022-01-31 15:33:35 +0200

    Update Graph.java

--- ex4/src/GRAPH/Graph.java
+++ ex4/src/GRAPH/Graph.java
@@ -1,1 +1,25 @@
+/***********/
+/* PACKAGE */
+/***********/
+package GRAPH;
 
+/*******************/
+/* GENERAL IMPORTS */
+/*******************/
+
+/*******************/
+/* PROJECT IMPORTS */
+/*******************/
+
+public class GRAPH
+{
+
+  public NodeList nodes;
+	public Graph() {
+    this.nodes = new NodeList(null, null);
+  }
+  
+  public void createBlocks(){
+    
+  }
+}
