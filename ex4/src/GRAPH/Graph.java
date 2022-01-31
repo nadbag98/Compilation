ff4ce787b1de111698177c3b30fad0c874b3962e
eyalban <79798517+eyalban@users.noreply.github.com>
2022-01-31 16:56:25 +0200

    Update Graph.java

--- ex4/src/GRAPH/Graph.java
+++ ex4/src/GRAPH/Graph.java
@@ -24,13 +24,35 @@ public class Graph
   }
   
   public void createBlocks(){
+	  Node prevNode = null;
 	  int counter = 1;
    	 BufferedReader reader;
 	 reader = new BufferedReader(new FileReader("..\..\output\MIPS.txt"));
 	  String line = reader.readLine();
 	  while(line != null){
 		  if (line.contains(":") && !line.contains(".word") && !line.contains(".asciiz")) {
-			this.nodes.add(counter, line.substring(0, line.length()-1));
+			  if (prevNode != null){
+				  prevNode.lastLine = counter-1;
+			  }
+			prevNode = this.nodes.add(counter, line.substring(0, line.length()-1));
+		  }
+		  else if(line.contains("\tj ") || line.contains("\tjal") || line.contains("\tblt") || line.contains("\tbgt") || line.containts("\tbge") ||
+			  line.contains("\tble") || line.contains("\tbne") || line.contains("\tbeq")){
+				line = reader.readLine();
+			  	counter++;
+			  	if (prevNode != null){
+				  prevNode.lastLine = counter-1;
+				  }
+			  	if (line.contains(":"){
+					prevNode = this.nodes.add(counter, line.substring(0, line.length()-1));
+				}
+				else{
+					prevNode = this.nodes.add(counter, null);	
+				}
+		  }
+		  else if(line.contains("\tjr")){
+			  prevNode.lastLine = counter;
+			prevNode = null;	  
 		  }
 		  
 		  line = reader.readLine();
