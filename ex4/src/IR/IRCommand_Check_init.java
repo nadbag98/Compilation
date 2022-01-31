bccad18659257ab31d524d6f405b2979ae277e0e
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2022-01-31 10:45:51 +0200

    Create IRCommand_Check_init.java

---
+++ ex4/src/IR/IRCommand_Check_init.java
@@ -0,0 +1,38 @@
+/***********/
+/* PACKAGE */
+/***********/
+package IR;
+
+/*******************/
+/* GENERAL IMPORTS */
+/*******************/
+
+/*******************/
+/* PROJECT IMPORTS */
+/*******************/
+import TEMP.*;
+import MIPS.*;
+
+public class IRCommand_Check_init extends IRcommand
+{
+	TEMP t;
+	
+	public IRCommand_Check_init(TEMP t)
+	{
+		this.t          = t;
+	}
+	
+	/***************/
+	/* MIPS me !!! */
+	/***************/
+	public void MIPSme()
+	{
+    String label_end        = getFreshLabel("end");
+		MIPSGenerator.getInstance().bnez(t,label_end);
+    MIPSGenerator.getInstance().la(t,"_null_ref");
+    MIPSGenerator.getInstance().print_string(t);
+    MIPSGenerator.getInstance().li("$v0",10);
+    MIPSGenerator.getInstance().syscall();
+    MIPSGenerator.getInstance().label(label_end);
+	}
+}
