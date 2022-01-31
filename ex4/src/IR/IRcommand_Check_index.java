9c65b5cabc5d9b45bdbe4fd10eab554ca425b508
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2022-01-31 12:17:23 +0200

    Update IRcommand_Check_index.java

--- ex4/src/IR/IRcommand_Check_index.java
+++ ex4/src/IR/IRcommand_Check_index.java
@@ -16,7 +16,7 @@ import MIPS.*;
 public class IRcommand_Check_index extends IRcommand
 {
 	TEMP addr;
-  TEMP idx;
+  	TEMP idx;
 	
 	public IRcommand_Check_index(TEMP addr, TEMP idx)
 	{
@@ -29,18 +29,18 @@ public class IRcommand_Check_index extends IRcommand
 	/***************/
 	public void MIPSme()
 	{
-    String label_end        = getFreshLabel("end");
-    String label_error        = getFreshLabel("error");
- 		MIPSGenerator.getInstance().bltz(idx,label_error);
-    MIPSGenerator.getInstance().load("$s0",this.addr, 0);
-    MIPSGenerator.getInstance().bge(idx,"$s0",label_error);
-    MIPSGenerator.getInstance().jump(label_end);
-    MIPSGenerator.getInstance().label(label_error);
-    MIPSGenerator.getInstance().la(t,"_acc_violation");
-    MIPSGenerator.getInstance().print_string(t);
-    MIPSGenerator.getInstance().li("$v0",10);
-    MIPSGenerator.getInstance().syscall(); 
-    MIPSGenerator.getInstance().label(label_end);
+	    String label_end        = getFreshLabel("end");
+	    String label_error        = getFreshLabel("error");
+ 	    MIPSGenerator.getInstance().bltz(this.idx,label_error);
+	    MIPSGenerator.getInstance().load("$s0",this.addr, 0);
+	    MIPSGenerator.getInstance().bge(this.idx,"$s0",label_error);
+	    MIPSGenerator.getInstance().jump(label_end);
+	    MIPSGenerator.getInstance().label(label_error);
+	    MIPSGenerator.getInstance().la(this.idx,"_acc_violation");
+	    MIPSGenerator.getInstance().print_string(this.idx);
+	    MIPSGenerator.getInstance().li("$v0",10);
+	    MIPSGenerator.getInstance().syscall(); 
+	    MIPSGenerator.getInstance().label(label_end);
     
     
 	}
