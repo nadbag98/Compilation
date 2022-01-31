0ba107c907f969213488aeea920a484f2d67cec3
eyalban <79798517+eyalban@users.noreply.github.com>
2022-01-31 17:58:43 +0200

    Update NodeList.java

--- ex4/src/GRAPH/NodeList.java
+++ ex4/src/GRAPH/NodeList.java
@@ -1,26 +1,74 @@
 public class NodeList
 {
-  public Node head;
-  public NodeList tail;
+  	public Node head;
+  	public NodeList tail;
   
 	public NodeList(Node head, NodeList tail) {
     this.head = head;
     this.tail = tail;
-  }
+ 	}
 	
-	public Node add(int line, String name){
-		if (this.head == null)
-			Node n = New Node(line, name);{
+	public void add(Node n){
+		if (this.head == null){
 			this.head = n;
-			return n;
+			return;
 		}
 		if (this.tail == null){
-			Node n = New Node(line, name);
 			this.tail = new NodeList(n, null);
-			return n;
+			return;
 		}
-		return this.tail.add(line, name);
+		this.tail.add(n);
+		return;
 	}
-  
 
+	public Node findByLast(int line){
+		if (this.head == null){
+			System.out.println("help me nodelis last\n");
+			return null;
+		}
+		if (this.head.lastLine == line){
+			return this.head;
+		}
+		else if(this.tail != null){
+			return this.tail.findByLast(line);
+		}
+		else{
+			System.out.println("help me nodelist no tail last :(\n");
+			return null;
+		}
+	}
+
+	public Node findByFirst(int line){
+		if (this.head == null){
+			System.out.println("help me nodelist first\n");
+			return null;
+		}
+		if (this.head.line == line){
+			return this.head;
+		}
+		else if(this.tail != null){
+			return this.tail.findByFirst(line);
+		}
+		else{
+			System.out.println("help me nodelist no tail first :(\n");
+			return null;
+		}
+	}
+
+	public Node findByLabel(String label){
+		if (this.head == null){
+			System.out.println("help me nodelist label\n");
+			return null;
+		}
+		if (this.head.label.equals(label)){
+			return this.head;
+		}
+		else if(this.tail != null){
+			return this.tail.findByLabel(label);
+		}
+		else{
+			System.out.println("help me nodelist no tail label :(\n");
+			return null;
+		}
+	}
 }
