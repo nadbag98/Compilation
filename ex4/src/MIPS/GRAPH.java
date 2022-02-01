6f7cb0c9de3580821dd36203b9efe1b0d0b6ae7e
eyalban <79798517+eyalban@users.noreply.github.com>
2022-02-01 17:20:25 +0200

    Update GRAPH.java

--- ex4/src/MIPS/GRAPH.java
+++ ex4/src/MIPS/GRAPH.java
@@ -21,19 +21,22 @@ public class GRAPH
 
   public int[][] liveArr;
   public int[][] tempArr;
+  public int[][] interMat;
+  public int[][] interMat2;
+  public int[] colors;
 
   public NodeList nodes;
   
   public GRAPH() {
     this.nodes = new NodeList(null, null);
-    this.tempArr = null;
+    this.tempCounter = 0;
   }
   
   public void createBlocks() throws IOException, FileNotFoundException{
     Node prevNode = null;
     int counter = 1;
     BufferedReader reader;
-   reader = new BufferedReader(new FileReader("output/MIPS.txt")); 
+   reader = new BufferedReader(new FileReader("output/MIPS_PRE_OPT.txt")); 
     String line = reader.readLine();
     while(line != null){
       if (line.contains(":") && !line.contains(".word") && !line.contains(".asciiz")) {
@@ -80,7 +83,7 @@ public class GRAPH
   public void createEdges() throws IOException, FileNotFoundException{
     int counter = 1;
     BufferedReader reader;
-    reader = new BufferedReader(new FileReader("output/MIPS.txt"));
+    reader = new BufferedReader(new FileReader("output/MIPS_PRE_OPT.txt"));
     String line = reader.readLine();
     while(line != null){
 
@@ -165,19 +168,29 @@ public class GRAPH
     
       if (temp_idx < first_comma_idx){    
         temps[0] = Integer.parseInt(line.substring(temp_idx+5, first_comma_idx));
+        if (temps[0] > this.tempCounter){
+          this.tempCounter = temps[0];
+        }
       }
 
       temp_idx = line.substring(first_comma_idx+1, line.length()).indexOf("Temp_") + first_comma_idx+1;
       if ((temp_idx > first_comma_idx) && (temp_idx < second_comma_idx)){
         temps[1] = Integer.parseInt(line.substring(temp_idx+5, second_comma_idx));
+        if (temps[1] > this.tempCounter){
+          this.tempCounter = temps[1];
+        }
       }
 
       temp_idx = line.substring(second_comma_idx, line.length()).indexOf("Temp_") + second_comma_idx ;
       if (temp_idx > second_comma_idx){
         temps[2] = Integer.parseInt(line.substring(temp_idx+5, line.length()));
+        if (temps[2] > this.tempCounter){
+          this.tempCounter = temps[2];
+        }
       }
       return temps;
   }
+
 
   public void liveAnalysis(){
     boolean changed = true;
@@ -248,4 +261,102 @@ public class GRAPH
     return changed;
   }
 
+
+  public void tempGraph(){
+    this.interMat = new int[this.tempCounter][this.tempCounter];
+    this.interMat2 = new int[this.tempCounter][this.tempCounter];
+    for (int[] line : this.liveArr){
+      for (int u : line){
+        for (int v : line){
+          this.interMat[u][v] = 1;
+          this.interMat[v][u] = 1;
+          this.interMat2[u][v] = 1;
+          this.interMat2[v][u] = 1;
+        }
+      }
+    }
+  }
+
+
+  public void colorGraph(){
+    int[] stack = new int[this.tempCounter];
+    int stack_idx = 0;
+    int prev = 0;
+    while (stack_idx < this.tempCounter){
+      prev = findNode2Remove(prev);
+      stack[stack_idx] = prev;
+      stack_idx++;
+      for (int i=0; i<this.tempCounter; i++){
+        this.interMat2[i][prev] = 0;
+        this.interMat2[prev][i] = 0;
+      }
+    }
+
+    this.colors = new int[this.tempCounter];
+    boolean[] takenColors;
+    for (int i=stack_idx; i>=0; i--){
+        takenColors = new boolean[10];
+
+        //update interMat2
+        this.interMat2[i][i] = 1;
+        for (int j=0; j<this.tempCounter; j++){
+          int is_neighbor = this.interMat2[j][j] * this.interMat[i][j];
+          this.interMat2[i][j] = is_neighbor;
+          this.interMat2[j][i] = is_neighbor;
+          if (is_neighbor == 1){
+            takenColors[this.colors[j]] = true;
+          }
+        }
+        for (int j=0; j<10; j++){
+          if (!takenColors[j]){
+            this.colors[i] = j;
+            break;
+          }
+        }
+    }
+  }
+
+
+  public int findNode2Remove(int idx){
+    for (int i=0; i<this.tempCounter; i++){
+      int degree = 0;
+      int j = (i + idx) % this.tempCounter;
+      for (int k=0; k<this.tempCounter; k++){
+        degree += this.interMat2[j][k];
+      }
+      if (degree > 0 && degree <= 10){
+        return j;
+      }
+    }
+    return -1;
+  }
+
+
+  public void optimizeTemps(PrintWriter fileWriter) throws IOException, FileNotFoundException{
+    int counter = 0;
+    int[] temps;
+
+    BufferedReader reader;
+    reader = new BufferedReader(new FileReader("output/MIPS_PRE_OPT.txt")); 
+    String line = reader.readLine();
+
+    while(line != null){ 
+      if (!line.contains("Temp_")){
+        fileWriter.print(line + "\n");
+        continue;
+      }
+      temps = this.tempArr[counter];
+      for (int i=0; i<3; i++){
+        if (temps[i] != -1){
+          line = line.replace(String.format("Temp_%d", temps[i]), String.format("$t%d", this.colors[temps[i]]));
+        }
+      }
+      fileWriter.print(line + "\n");
+
+      counter++;
+      line = reader.readLine();
+    }
+
+    reader.close();
+  }
 }
