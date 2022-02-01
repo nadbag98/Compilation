5018c8fe12cb8866c07e3cc842bf4e477a8c5750
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2022-02-01 20:08:28 +0200

    Update MIPSGenerator.java

--- ex4/src/MIPS/MIPSGenerator.java
+++ ex4/src/MIPS/MIPSGenerator.java
@@ -386,22 +386,22 @@ public class MIPSGenerator
 		
 		fileWriter.format("\tli $s0,1\n");
 		fileWriter.format("\tmove $s1,Temp_%d\n", t1_ind);
-		fileWriter.format("\t%s:\n", str1_len_loop);
+		fileWriter.format("%s:\n", str1_len_loop);
 		fileWriter.format("\tlb $s2, 0($s1)\n");
 		fileWriter.format("\tbeq $s2, 0, %s\n", str1_len_end);
 		fileWriter.format("\taddu $s0, $s0, 1\n");
 		fileWriter.format("\taddu $s1, $s1, 1\n");
-		fileWriter.format("\tj%s\n", str1_len_loop);
-		fileWriter.format("\t%s:\n", str1_len_end);
+		fileWriter.format("\tj %s\n", str1_len_loop);
+		fileWriter.format("%s:\n", str1_len_end);
 		
 		fileWriter.format("\tmove $s1,Temp_%d\n", t2_ind);
-		fileWriter.format("\t%s:\n", str2_len_loop);
+		fileWriter.format("%s:\n", str2_len_loop);
 		fileWriter.format("\tlb $s2, 0($s1)\n");
 		fileWriter.format("\tbeq $s2, 0, %s\n", str2_len_end);
 		fileWriter.format("\taddu $s0, $s0, 1\n");
 		fileWriter.format("\taddu $s1, $s1, 1\n");
-		fileWriter.format("\tj%s\n", str2_len_loop);
-		fileWriter.format("\t%s:\n", str2_len_end);
+		fileWriter.format("\tj %s\n", str2_len_loop);
+		fileWriter.format("%s:\n", str2_len_end);
 		//$s0 holds the concated string length, including null terminator
 		
 		fileWriter.format("\tli $v0, 9\n");
@@ -412,25 +412,25 @@ public class MIPSGenerator
 		//$s0 holds the address for the new string
 		
 		fileWriter.format("\tmove $s1,Temp_%d\n", t1_ind);
-		fileWriter.format("\t%s:\n", copy_str1_loop);
+		fileWriter.format("%s:\n", copy_str1_loop);
 		fileWriter.format("\tlb $s2, 0($s1)\n");
 		fileWriter.format("\tbeq $s2, 0, %s\n", copy_str1_end);
 		fileWriter.format("\tsb $s2, 0($s0)\n");
 		fileWriter.format("\taddu $s0, $s0, 1\n");
 		fileWriter.format("\taddu $s1, $s1, 1\n");
-		fileWriter.format("\tj%s\n", copy_str1_loop);
-		fileWriter.format("\t%s:\n", copy_str1_end);
+		fileWriter.format("\tj %s\n", copy_str1_loop);
+		fileWriter.format("%s:\n", copy_str1_end);
 		
 		
 		fileWriter.format("\tmove $s1,Temp_%d\n", t2_ind);
-		fileWriter.format("\t%s:\n", copy_str2_loop);
+		fileWriter.format("%s:\n", copy_str2_loop);
 		fileWriter.format("\tlb $s2, 0($s1)\n");
 		fileWriter.format("\tbeq $s2, 0, %s\n", copy_str2_end);
 		fileWriter.format("\tsb $s2, 0($s0)\n");
 		fileWriter.format("\taddu $s0, $s0, 1\n");
 		fileWriter.format("\taddu $s1, $s1, 1\n");
-		fileWriter.format("\tj%s\n", copy_str2_loop);
-		fileWriter.format("\t%s:\n", copy_str2_end);
+		fileWriter.format("\tj %s\n", copy_str2_loop);
+		fileWriter.format("%s:\n", copy_str2_end);
 		fileWriter.format("\taddu $s0, $s0, 1\n");
 		fileWriter.format("\tsb $s2, 0($s0)\n");
 	}
