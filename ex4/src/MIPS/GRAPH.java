3645bd261074ef28ecc538aea64b9192ca890cc1
nadbag98 <63659269+nadbag98@users.noreply.github.com>
2022-02-01 17:53:18 +0200

    Update GRAPH.java

--- ex4/src/MIPS/GRAPH.java
+++ ex4/src/MIPS/GRAPH.java
@@ -274,7 +274,6 @@ public class GRAPH
       for (int u : line){
         for (int v : line){
           if (u != -1 && v != -1){
-            System.out.println(String.format("%d", this.tempCounter));
             this.interMat[u][v] = 1;
             this.interMat[v][u] = 1;
             this.interMat2[u][v] = 1;
