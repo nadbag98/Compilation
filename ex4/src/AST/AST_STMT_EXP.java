69886573b5b1be3a6f2a3ee50b2a7b7e5463bc8c
Eleyah1 <92672233+Eleyah1@users.noreply.github.com>
2022-02-08 21:24:14 +0200

    Update AST_STMT_EXP.java

--- ex4/src/AST/AST_STMT_EXP.java
+++ ex4/src/AST/AST_STMT_EXP.java
@@ -73,9 +73,6 @@ public class AST_STMT_EXP extends AST_STMT
 				throw new ArithmeticException(String.format("%d", e.line));
 			}
 			sym_table.beginScope();
-			if (this.is_while == 1){
-				sym_table.top.isWhile = true;
-			}
 			sym_table.top.offset = sym_table.top.prevtop.offset;
 			this.l.visit(sym_table, returnType);
 			sym_table.endScope();
